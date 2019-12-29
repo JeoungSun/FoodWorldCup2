1880ea0fa830f65a1a5aa806bc2f6c58f25162c5
JeongSun <jslee2769@naver.com>
2019-12-29 19:32:23 +0900

    new

--- app/src/main/java/com/ljs/practice/foodworldcup/MainActivity.java
+++ app/src/main/java/com/ljs/practice/foodworldcup/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ljs.practice.foodworldcup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -8,7 +10,7 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String TAG = "MainActivity";
    int[] imgs = {R.drawable.food1, R.drawable.food2,R.drawable.food3,R.drawable.food4,R.drawable.food5,R.drawable.food6,
             R.drawable.food7,R.drawable.food8,R.drawable.food9,R.drawable.food10,R.drawable.food11,R.drawable.food12,R.drawable.food13,
             R.drawable.food14,R.drawable.food15,R.drawable.food16};
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                if(count>15){
                    r8[a] = count - 2;
                    count=0;
+                   intent1.putExtra("key", r8);
                    startActivity(intent1);
                }
                image1.setImageResource(imgs[count]);
@@ -48,9 +51,12 @@ public class MainActivity extends AppCompatActivity {
            Intent intent1 = new Intent(getApplicationContext(), round8.class);
            @Override
            public void onClick(View view) {
+
                if(count>15){
                   r8[a] = count - 1;
+                   Log.d(TAG, "onClick: "+ r8[7]);
                    count=0;
+                   intent1.putExtra("key", r8);
                    startActivity(intent1);
                }
                image1.setImageResource(imgs[count]);
